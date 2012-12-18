99d5934e360d83b7a1bb0a2f85c7038414294f71
Christopher Wellons <mosquitopsu@gmail.com>
2012-12-17 21:58:01 -0500

    Initial commit of working code.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elisp Pastebin
+
+This is a demo of simple-httpd implementing a pastebin.
---
+++ pastebin.el
@@ -0,0 +1,26 @@
+;;; pastebin.el --- pastebin example with simple-httpd
+
+(require 'cl)
+(require 'url-util)
+(require 'simple-httpd)
+
+(defvar pastebin-data-root (file-name-directory load-file-name))
+
+(defvar pastebin-db (make-hash-table))
+
+(defun pastebin-get-file (file)
+  (with-temp-buffer
+    (insert-file-literally (expand-file-name file pastebin-data-root))
+    (buffer-string)))
+
+(defservlet pastebin text/html (path)
+  (let* ((id (string-to-number (file-name-nondirectory path)))
+         (content (url-insert-entities-in-string (gethash id pastebin-db ""))))
+    (insert (format (pastebin-get-file "paste.html") content))))
+
+(defun httpd/pastebin/post (proc path query request)
+  (let* ((id (random most-positive-fixnum))
+         (content (cadr (assoc "Content" request)))
+         (decode (url-unhex-string (substitute ?  ?+ content) t)))
+    (puthash id (substring decode 2) pastebin-db)
+    (httpd-redirect proc (format "/pastebin/%d" id))))
