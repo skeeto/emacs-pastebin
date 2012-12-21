a7e64ffeeac0e78849459de78d356c58f2284f03
Christopher Wellons <mosquitopsu@gmail.com>
2012-12-21 11:06:39 -0500

    Better paste IDs.

--- pastebin.el
+++ pastebin.el
@@ -6,7 +6,30 @@
 
 (defvar pastebin-data-root (file-name-directory load-file-name))
 
-(defvar pastebin-db (make-hash-table))
+(defvar pastebin-db (make-hash-table :test 'equal))
+
+(defvar pastebin-id-digits
+  "0123456789abcedfghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ-.")
+
+(defun pastebin-id-valid-p (id)
+  (not (or (gethash id pastebin-db)
+           (string-match-p "^[.-]\\|[.-]$\\|\\.-\\|-\\." id))))
+
+(defun* pastebin-make-id (&optional (min-length 4))
+  (loop with base = (length pastebin-id-digits)
+        for length upfrom min-length
+        for digits = (loop repeat length
+                           collect (aref pastebin-id-digits (random base)))
+        for id = (coerce digits 'string)
+        when (pastebin-id-valid-p id) return id))
+
+(defun* pastebin-make-id (&optional (min-length 4))
+  (flet ((make-digit ()
+           (aref pastebin-id-digits (random (length pastebin-id-digits)))))
+    (loop for length upfrom min-length
+          for digits = (loop repeat length collect (make-digit))
+          for id = (coerce digits 'string)
+          when (pastebin-id-valid-p id) return id)))
 
 (defun pastebin-get-file (file)
   (with-temp-buffer
@@ -14,13 +37,13 @@
     (buffer-string)))
 
 (defservlet pastebin text/html (path)
-  (let* ((id (string-to-number (file-name-nondirectory path) 16))
+  (let* ((id (file-name-nondirectory path))
          (content (url-insert-entities-in-string (gethash id pastebin-db ""))))
     (insert (format (pastebin-get-file "paste.html") content))))
 
 (defun httpd/pastebin/post (proc path query request)
-  (let* ((id (random most-positive-fixnum))
+  (let* ((id (pastebin-make-id))
          (content (cadr (assoc "Content" request)))
          (decode (url-unhex-string (substitute ?  ?+ content) t)))
     (puthash id (substring decode 2) pastebin-db)
-    (httpd-redirect proc (format "/pastebin/%x" id))))
+    (httpd-redirect proc (format "/pastebin/%s" id))))
