59cd53949cfd5e808decd57180645d23d7ad04eb
Christopher Wellons <mosquitopsu@gmail.com>
2012-12-21 12:43:59 -0500

    Use jQuery chaining, you dolt.

--- pastebin.js
+++ pastebin.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
     var id = location.pathname.split('/').slice(-1)[0];
     if (id) {
-        $('#paste').show();
         $.get('get/' + id, function(entry) {
-            $('#paste').text(entry.content);
+            $('#paste').show().text(entry.content);
             $('#content').val(entry.content);
         }, 'json');
     }
