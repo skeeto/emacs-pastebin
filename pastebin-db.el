f22a0aed0cea753a2d476a89e8771a50b473a829
Christopher Wellons <mosquitopsu@gmail.com>
2012-12-22 19:05:40 -0500

    Add a flat-file database.

--- pastebin-db.el
+++ pastebin-db.el
@@ -44,4 +44,31 @@
 (defmethod pastebin-db-put ((db db-hash-table) id entry)
   (puthash id entry (slot-value db 'table)))
 
+;; Flat-file database
+
+(defclass db-flat-file ()
+  ((directory :initarg :directory))
+  (:documentation "Entries are stored as individual files in a directory."))
+
+(defun make-db-flat-file (root-directory)
+  "Make a new flat-file database using ROOT-DIRECTORY as storage."
+  (make-instance 'db-flat-file :directory root-directory))
+
+(defun db-flat-file--clean (id)
+  (replace-regexp-in-string "[/~]\\|\\.\\." "" id))
+
+(defmethod pastebin-db-get ((db db-flat-file) id)
+  (let* ((root (slot-value db 'directory))
+         (file (expand-file-name (db-flat-file--clean id) root)))
+    (when (file-exists-p file)
+      (with-temp-buffer
+        (insert-file-contents-literally file)
+        (read (current-buffer))))))
+
+(defmethod pastebin-db-put ((db db-flat-file) id entry)
+  (let* ((root (slot-value db 'directory))
+         (file (expand-file-name (db-flat-file--clean id) root)))
+    (with-temp-file file
+      (prin1 entry (current-buffer)))))
+
 (provide 'pastebin-db)
